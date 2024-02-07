6d60d06d96d98c66bb01b3dd9ec7fd0a468540f8
dacenkoa <annadacenkomail@gmail.com>
2024-02-07 10:43:28 +0100

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('.top__slider , .bike__slider-inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  });
+
+  $('.form__choice-radio').on('click', function () {
+    $('.form__choice-item').removeClass('img__active');
+    $(this).parent().addClass('img__active');
+  });
+
+  $('.burger__menu').on('click', function () {
+    $('.header__menu').toggleClass('show');
+    $('.burger__menu').toggleClass('show');
+    $('.bike__slider').toggleClass('show');
+  });
+});
