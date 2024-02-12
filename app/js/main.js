f2959777cf26f016f121b65c95e290297c90fe49
dacenkoa <125560792+dacenkoa@users.noreply.github.com>
2024-02-12 21:32:41 +0100

    added dark and light site theme

--- app/js/main.js
+++ app/js/main.js
@@ -17,16 +17,34 @@ $(document).ready(function () {
     $('.top').toggleClass('show');
   });
 
-  $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-          $('.scrollToTopBtn').addClass('showBtn');
-      } else {
-          $('.scrollToTopBtn').removeClass('showBtn');
-      }
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTopBtn').addClass('showBtn');
+    } else {
+      $('.scrollToTopBtn').removeClass('showBtn');
+    }
   });
-  
-  $('.scrollToTopBtn').click(function(){
-      $('html, body').animate({scrollTop : 0},800);
-      return false;
+
+  $('.scrollToTopBtn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
   });
+
+
+  // По кліку на кнопку змінюємо тему
+  $('#theme-toggle').click(function () {
+    // Перевіряємо поточну тему та змінюємо її
+    if ($('body').hasClass('light-mode')) {
+      $('body').removeClass('light-mode').addClass('dark-mode');
+      $('.header__link-img').attr("src", "../images/logo_img-dark.jpg");
+      $('.bx').removeClass('bxl-youtube').addClass('bxl-instagram');
+
+    } else {
+      $('body').removeClass('dark-mode').addClass('light-mode');
+      $('.header__link-img').attr("src", "../images/logo_img.jpg");
+      $('.bx').removeClass('bxl-instagram').addClass('bxl-youtube');
+    }
+
+  });
+
 });
